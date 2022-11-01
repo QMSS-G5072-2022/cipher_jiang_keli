7d7cf7272b28a51023a23152551793dc7f346dcc
kkkk-jkl <kjiang@wesleyan.edu>
2022-11-01 16:39:28 -0400

    update test

--- cipher_kj2592/tests/test_cipher_kj2592.py
+++ cipher_kj2592/tests/test_cipher_kj2592.py
@@ -3,5 +3,5 @@ from cipher_kj2592 import cipher_kj2592
 def test_cipher():
     example = 'abcd'    
     expected = 'ABCD'
-    actual = cipher(example, 26)
+    actual = cipher_kj2592.cipher(example, 26)
     assert actual == expected
